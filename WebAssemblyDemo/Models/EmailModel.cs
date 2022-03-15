ba0cce897e72e6988c2f186bb987259ac2bc6b38
liero <turand@centrum.sk>
2022-03-15 17:19:51 +0100

    Upgraded to .NET 6

--- WebAssemblyDemo/Models/EmailModel.cs
+++ WebAssemblyDemo/Models/EmailModel.cs
@@ -1,8 +1,6 @@
-﻿using FluentValidation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿#nullable disable
+
+using FluentValidation;
 
 namespace WebAssemblyDemo.Models
 {
--- WebAssemblyDemo/Models/Person.cs
+++ WebAssemblyDemo/Models/Person.cs
@@ -1,15 +1,11 @@
 ﻿using FluentValidation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebAssemblyDemo.Models
 {
     public class Person
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
         public int? Age { get; set; }
         public Address Address { get; set; } = new Address();
     }
